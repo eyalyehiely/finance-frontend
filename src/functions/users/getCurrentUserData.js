c464d6c634b89b6014f5d442607a663e60c41c25
eyalyehiely <eyalwork0@gmail.com>
2024-07-04 17:51:11 +0300

    remove alertf from get current user data

--- src/functions/users/getCurrentUserData.js
+++ src/functions/users/getCurrentUserData.js
@@ -1,7 +1,5 @@
 
 import axios from '../axiosConfig'
-import swal from 'sweetalert';
-
 
 export default function getCurrentUserData(token,setUser) {
 
@@ -16,20 +14,8 @@ export default function getCurrentUserData(token,setUser) {
         
       } else {
         console.log('Error:', response.data.message);
-        swal({
-          title: "Ⅹ!שגיאה ",
-          text: `שגיאת frontend: ${response.data.message}`,
-          icon: "warning",
-          button: "אישור",
-        });
       }
     }).catch(error => {
       console.error('There was an error!', error);
-      swal({
-        title: "שגיאה!",
-        text: "שגיאת BACKEND",
-        icon: "warning",
-        button: "אישור",
-      });
     });
   }
