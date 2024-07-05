cc993acb5122bd18f7ff438de91d9a60e77ea38b
eyalyehiely <eyalwork0@gmail.com>
2024-07-05 23:54:49 +0300

    fix welcome banner card

--- src/partials/dashboard/WelcomeBanner.jsx
+++ src/partials/dashboard/WelcomeBanner.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Note from '../../components/UserNote';
-import {jwtDecode} from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 
 function WelcomeBanner() {
-  const first_name = jwtDecode(localStorage.getItem('authTokens')).first_name;
+  const token = localStorage.getItem('authTokens');
+  const first_name = token ? jwtDecode(token).first_name : '';
+
   return (
     <div className="relative bg-indigo-200 dark:bg-indigo-800 p-4 sm:p-6 rounded-full overflow-hidden mb-8">
       {/* Content */}
-      <div dir="rtl" className="relative">
-        <h2 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">
-          <Note /> {first_name} 👋
+      <div dir="rtl" className="relative flex items-center">
+        <h2 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1 flex items-center">
+          <Note />
+          <span className="ml-2">{first_name}</span>
         </h2>
       </div>
     </div>
