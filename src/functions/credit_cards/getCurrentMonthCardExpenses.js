1fa2bc3da7aed0c29213f2d951c025296e622367
eyalyehiely <eyalwork0@gmail.com>
2024-07-05 23:54:06 +0300

    add credit expenses

---
+++ src/functions/credit_cards/getCurrentMonthCardExpenses.js
@@ -0,0 +1,22 @@
+import axios from '../axiosConfig'
+
+export default function getCurrentMonthCardExpenses(token,setExpenses) {
+    // event.preventDefault();
+    
+    axios.post('/expenses/fetch_user_expenses/',{},{
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }
+  }).then(response => {
+        if (response.data.status ===200) {
+            console.log({'all_expenses':response.data.all_expenses});
+            setExpenses(response.data.credit_card); 
+        } else {
+            console.log('Error:', response.data.message);
+        }
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+  });
+}
