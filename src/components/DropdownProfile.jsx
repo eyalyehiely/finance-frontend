b8e1490a1d719b99fcf0cc8385f455a785fa645e
eyalyehiely <eyalwork0@gmail.com>
2024-07-06 16:41:36 +0300

    fixing app icon

--- src/components/DropdownProfile.jsx
+++ src/components/DropdownProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link,NavLink } from 'react-router-dom';
 import Transition from '../utils/Transition';
 import LogoutButton from './LogOut';
 import { jwtDecode } from 'jwt-decode';
@@ -54,12 +54,25 @@ function DropdownProfile({
         onClick={() => setDropdownOpen(!dropdownOpen)}
         aria-expanded={dropdownOpen}
       >
-        <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
+        <NavLink end to="#" className="block">
+            <svg
+              className="w-8 h-8" // Ensures 32x32 size
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 235.517 235.517"
+              fill="#00000" // White fill color
+              aria-hidden="true"
+            >
+              <path
+                style={{ fill: '#0000000' }} // White fill color
+                d="M118.1,235.517c7.898,0,14.31-6.032,14.31-13.483c0-7.441,0-13.473,0-13.473 c39.069-3.579,64.932-24.215,64.932-57.785v-0.549c0-34.119-22.012-49.8-65.758-59.977V58.334c6.298,1.539,12.82,3.72,19.194,6.549 c10.258,4.547,22.724,1.697,28.952-8.485c6.233-10.176,2.866-24.47-8.681-29.654c-11.498-5.156-24.117-8.708-38.095-10.236V8.251 c0-4.552-6.402-8.251-14.305-8.251c-7.903,0-14.31,3.514-14.31,7.832c0,4.335,0,7.843,0,7.843 c-42.104,3.03-65.764,25.591-65.764,58.057v0.555c0,34.114,22.561,49.256,66.862,59.427v33.021 c-10.628-1.713-21.033-5.243-31.623-10.65c-11.281-5.755-25.101-3.72-31.938,6.385c-6.842,10.1-4.079,24.449,7.294,30.029 c16.709,8.208,35.593,13.57,54.614,15.518v13.755C103.79,229.36,110.197,235.517,118.1,235.517z M131.301,138.12 c14.316,4.123,18.438,8.257,18.438,15.681v0.555c0,7.979-5.776,12.651-18.438,14.033V138.12z M86.999,70.153v-0.549 c0-7.152,5.232-12.657,18.71-13.755v29.719C90.856,81.439,86.999,77.305,86.999,70.153z"
+              />
+            </svg>
+          </NavLink>
         <div className="flex items-center truncate">
           <span className="truncate ml-2 text-sm font-medium dark:text-slate-300 group-hover:text-slate-800 dark:group-hover:text-slate-200">{first_name}</span>
-          <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
+          {/* <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
-          </svg>
+          </svg> */}
         </div>
       </button>
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -118,33 +118,32 @@ function Signup() {
 
             {/* Header */}
             <div className="flex-1">
-              <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-                {/* Logo */}
-                
-                <svg width="32" height="32" viewBox="0 0 32 32">
-                  <defs>
-                    <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
-                      <stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
-                      <stop stopColor="#A5B4FC" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
-                      <stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
-                      <stop stopColor="#38BDF8" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <rect fill="#6366F1" width="32" height="32" rx="16" />
-                  <path
-                    d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z"
-                    fill="#4F46E5" />
-                  <path
-                    d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z"
-                    fill="url(#logo-a)" />
-                  <path
-                    d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z"
-                    fill="url(#logo-b)" />
-                </svg>
-              </div>
-            </div>
+                  <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+                    <Link className="block">
+                      {/* New SVG */}
+                      <svg 
+                        height="32" 
+                        width="32" 
+                        version="1.1" 
+                        id="Capa_1" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        xmlnsXlink="http://www.w3.org/1999/xlink" 
+                        viewBox="0 0 235.517 235.517" 
+                        xmlSpace="preserve" 
+                        fill="#000000"
+                      >
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <path 
+                            style={{ fill: '#010002' }} 
+                            d="M118.1,235.517c7.898,0,14.31-6.032,14.31-13.483c0-7.441,0-13.473,0-13.473 c39.069-3.579,64.932-24.215,64.932-57.785v-0.549c0-34.119-22.012-49.8-65.758-59.977V58.334c6.298,1.539,12.82,3.72,19.194,6.549 c10.258,4.547,22.724,1.697,28.952-8.485c6.233-10.176,2.866-24.47-8.681-29.654c-11.498-5.156-24.117-8.708-38.095-10.236V8.251 c0-4.552-6.402-8.251-14.305-8.251c-7.903,0-14.31,3.514-14.31,7.832c0,4.335,0,7.843,0,7.843 c-42.104,3.03-65.764,25.591-65.764,58.057v0.555c0,34.114,22.561,49.256,66.862,59.427v33.021 c-10.628-1.713-21.033-5.243-31.623-10.65c-11.281-5.755-25.101-3.72-31.938,6.385c-6.842,10.1-4.079,24.449,7.294,30.029 c16.709,8.208,35.593,13.57,54.614,15.518v13.755C103.79,229.36,110.197,235.517,118.1,235.517z M131.301,138.12 c14.316,4.123,18.438,8.257,18.438,15.681v0.555c0,7.979-5.776,12.651-18.438,14.033V138.12z M86.999,70.153v-0.549 c0-7.152,5.232-12.657,18.71-13.755v29.719C90.856,81.439,86.999,77.305,86.999,70.153z"
+                          />
+                        </g>
+                      </svg>
+                    </Link>
+                  </div>
+                </div>
 
             <div className="max-w-sm mx-auto w-full px-4 py-8">
               <Button type="button" href='/signin' variant="info">חזור</Button>
