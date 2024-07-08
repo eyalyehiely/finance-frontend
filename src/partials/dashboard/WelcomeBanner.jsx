08a7569debf0128f204130f6238a8af482b0054d
eyalyehiely <eyalwork0@gmail.com>
2024-07-09 01:19:07 +0300

    minimize header

--- src/partials/dashboard/WelcomeBanner.jsx
+++ src/partials/dashboard/WelcomeBanner.jsx
@@ -10,10 +10,10 @@ function WelcomeBanner() {
     <div className="relative bg-indigo-200 dark:bg-indigo-800 p-4 sm:p-6 rounded-full overflow-hidden mb-8">
       {/* Content */}
       <div dir="rtl" className="relative flex items-center">
-        <h1 className="text-2xl md:text-2xl text-slate-800 dark:text-slate-100 font-bold mb-1 flex items-center">
+        <h2 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1 flex items-center">
           <Note />
           <span className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1 flex items-center">, {first_name} 👋🏻</span>
-        </h1>
+        </h2>
       </div>
     </div>
   );
