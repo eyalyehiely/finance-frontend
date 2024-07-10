e06cc1b2a335585911e0bc936deebffb2d8d8eab
eyalyehiely <eyalwork0@gmail.com>
2024-07-11 00:25:14 +0300

    create file

---
+++ src/functions/users/sendResetPasswordMail.js
@@ -0,0 +1,38 @@
+
+import axios from '../axiosConfig'
+import swal from 'sweetalert';
+
+export default function fetchData(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+
+    axios.post('/auth/reset_password/', {
+      email: email,
+    }).then((response) => {
+      if (response.status === 200) {
+        swal({
+          title: "קישור נשלח בהצלחה",
+          icon: "success",
+          timer:1000,
+          button: false,
+        }).then(() => {
+          window.location.href = '/signin';
+        });
+      } else {
+        swal({
+          title: "משתמש לא נמצא",
+          icon: "warning",
+          button: "אישור",
+        });
+      }
+    }).catch(error => {
+      console.error('Error occurred:', error);
+      swal({
+        title: "שגיאה",
+        text: "משהו השתבש, אנא נסה שוב",
+        icon: "warning",
+        button: "אישור",
+      });
+    });
+  }
