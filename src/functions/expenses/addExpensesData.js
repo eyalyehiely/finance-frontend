45106f455c91d7ed81eeff88a1ac2df9a0f7550a
eyalyehiely <eyalwork0@gmail.com>
2024-07-27 12:58:03 +0300

    improving fetch expenses data func

--- src/functions/expenses/addExpensesData.js
+++ src/functions/expenses/addExpensesData.js
@@ -1,40 +1,3 @@
-// import axios from '../axiosConfig';
-// import swal from 'sweetalert';
-// import fetchExpensesData from './fetchExpensesData';
-
-// export default function addExpensesData(token, setExpenses, data, handleClose) {
-//   axios.post('/expenses/add_expense/', data, {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${token}`,
-//     },
-//   })
-//   .then((response) => {
-//     // Add the new expense to the state
-//     setExpenses((prevExpenses) => [...prevExpenses, response.data]);
-
-//     swal({
-//       title: " 💰!הוצאה נוספה בהצלחה",
-//       icon: "success",
-//       timer: 2000,
-//       button: false,
-//     }).then(() => {
-//       handleClose();
-//       fetchExpensesData(token, setExpenses);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error.response?.data?.message || error.message);
-//     swal({
-//       title: "Ⅹ!שגיאה ",
-//       text: "!שגיאת BACKEND",
-//       icon: "warning",
-//       button: "אישור",
-//     });
-//   });
-// }
-
-
 import axios from '../axiosConfig';
 import swal from 'sweetalert';
 import fetchExpensesData from './fetchExpensesData';
@@ -52,7 +15,7 @@ export default async function addExpensesData(token, setExpenses, data, handleCl
     setExpenses((prevExpenses) => [...prevExpenses, response.data]);
 
     swal({
-      title: " 💰!הוצאה נוספה בהצלחה",
+      title: "💰!הוצאה נוספה בהצלחה",
       icon: "success",
       timer: 2000,
       button: false,
@@ -64,7 +27,7 @@ export default async function addExpensesData(token, setExpenses, data, handleCl
   } catch (error) {
     console.error('Error:', error.response?.data?.message || error.message);
     swal({
-      title: "Ⅹ!שגיאה ",
+      title: "Ⅹ!שגיאה",
       text: error.response?.data?.message || "!שגיאת BACKEND",
       icon: "warning",
       button: "אישור",
